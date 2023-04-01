5754b858ee688ad8c43270d7512d94749e2ef262
lizhi <2318691019@qq.com>
2023-04-01 21:15:36 +0800

    init

--- src/main/java/com/lizhi/stpspringbootinit/StpSpringbootInitApplication.java
+++ src/main/java/com/lizhi/stpspringbootinit/StpSpringbootInitApplication.java
@@ -1,6 +1,4 @@
 package com.lizhi.stpspringbootinit;
-
-import cn.dev33.satoken.quick.SaQuickManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,9 +8,7 @@ public class StpSpringbootInitApplication {
     public static void main(String[] args) {
         SpringApplication.run(StpSpringbootInitApplication.class, args);
 
-        System.out.println("\n------ 启动成功 ------");
-        System.out.println("name: " + SaQuickManager.getConfig().getName());
-        System.out.println("pwd:  " + SaQuickManager.getConfig().getPwd());
+
 
     }
 
--- src/main/java/com/lizhi/stpspringbootinit/controller/UserController.java
+++ src/main/java/com/lizhi/stpspringbootinit/controller/UserController.java
@@ -20,12 +20,12 @@ import com.lizhi.stpspringbootinit.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.thymeleaf.util.Validate;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
