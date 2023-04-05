81962ab0e0ab66808781a49c0e2928994cb4143c
lizhi <2318691019@qq.com>
2023-04-05 23:51:41 +0800

    v1.3 add RedisUtil string 功能

--- src/main/java/com/lizhi/stpspringbootinit/controller/TestController.java
+++ src/main/java/com/lizhi/stpspringbootinit/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.lizhi.stpspringbootinit.controller;
 
 import cn.dev33.satoken.util.SaResult;
+import com.lizhi.stpspringbootinit.util.RedisUtil;
 import com.lizhi.stpspringbootinit.util.TooQrCode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author <a href="https://github.com/lizhe-0423">荔枝</a>
@@ -19,21 +26,51 @@ public class TestController {
 
     @Resource
     com.lizhi.stpspringbootinit.util.TooQrCode TooQrCode;
+
     @Resource
-    RedisTemplate<String,Object> stringRedisTemplate;
+    RedisTemplate<String,Object> redisTemplate;
+
+    @Resource
+    RedisUtil redisUtil;
 
 
-    @RequestMapping("get")
+    @RequestMapping("getTooQrCode")
     public void get(){
         TooQrCode.generateTooQrCode("http://hutool.cn/");
     }
 
-    @RequestMapping("redisSet")
+    @RequestMapping("setRedStr")
     public void redisSet(){
-        System.out.println("进入 redis 测试页面");
-        stringRedisTemplate.opsForValue().set("hello","hello,world");
-
+        redisUtil.setString("测试","测试");
+    }
+    @RequestMapping("getRedStr")
+    public String redisGet(){
+        String string = redisUtil.getString("测试");
+        return string;
+    }
+    @RequestMapping("setRedStrEx")
+    public void redisSetEx(){
+        redisUtil.setStringEx("测试2","测试2",10, TimeUnit.SECONDS);
     }
 
+
+    @RequestMapping("delRedStr")
+    public void delRedStr(){
+        redisUtil.del("测试");
+    }
+
+    @RequestMapping("incr")
+    @ApiOperation("自增")
+    public long redisIncr(){
+        long incr = redisUtil.incr("1641766009775976450", 1);
+        return incr;
+    }
+    @RequestMapping("decr")
+    @ApiOperation("自减")
+
+    public long redisDecr(){
+        long decr = redisUtil.decr("1641766009775976450", 1);
+        return decr;
+    }
 
 }
--- src/main/java/com/lizhi/stpspringbootinit/util/RedisUtil.java
+++ src/main/java/com/lizhi/stpspringbootinit/util/RedisUtil.java
@@ -1,18 +1,91 @@
 package com.lizhi.stpspringbootinit.util;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.apache.poi.ss.formula.functions.T;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author <a href="https://github.com/lizhe-0423">荔枝</a>
  * redis 操作
+ * string:缓存结构体信息(用户信息、登录信息、token)、计数功能(统计点赞、粉丝、关注数量)
  */
-
+@Component
 public class RedisUtil {
+
+    @Resource
+    RedisTemplate<String,Object> stringRedisTemplate;
+
+
+    /**
+     * redis 存储 string 类型
+     * @param str1 key
+     * @param str2 value
+     */
+    public void setString(@NotNull  String str1, String str2){
+        stringRedisTemplate.opsForValue().set(str1,str2);
+    }
+
+    /**
+     * redis 根据key查询 value
+     * @param str key
+     * @return
+     */
+    public String getString(@NotNull String str){
+        String o = (String) stringRedisTemplate.opsForValue().get(str);
+        return  o;
+    }
+
+    /**
+     * redis 设置string存储时间
+     * @param str1 key
+     * @param str2 value
+     * @param time int
+     * @param timeUnit 期效
+     */
+    public void  setStringEx(@NotNull String str1, String str2, int time, TimeUnit timeUnit){
+        stringRedisTemplate.opsForValue().set(str1,str2,time,timeUnit);
+    }
+
+    /**
+     * 根据key 删除操作
+     * @param string key
+     */
+    public void del(String string){
+        stringRedisTemplate.delete(string);
+    }
+
+    /**
+     * 根据nums 自增
+     * @param str key
+     * @param nums 自增
+     * @return
+     */
+    public Long incr(String str,int nums){
+        long increment = stringRedisTemplate.opsForValue().increment(str, nums);
+        return increment;
+    }
+
+    /**
+     * 根据nums 自减
+     * @param str
+     * @param nums
+     * @return
+     */
+    public Long decr(String str,int nums){
+        Long decrement = stringRedisTemplate.opsForValue().decrement(str, nums);
+        return decrement;
+    }
 
 
 
